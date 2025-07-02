f805a3460e25977c51635a2209ba540a38a01ccd
MenaceMaster9600 <jm9876000@gmail.com>
2025-07-02 18:38:25 -0400

    Deleted

--- Deleted/app.js
+++
@@ -1,9 +0,0 @@
-import { fetchImportData } from './github.js';
-
-const displayImportInfo = async () => {
-    const importData = await fetchImportData();
-    const importInfoDiv = document.getElementById('import-info');
-    importInfoDiv.innerHTML = importData.map(import => `<p>${import.title}</p>`).join('');
-};
-
-displayImportInfo();
