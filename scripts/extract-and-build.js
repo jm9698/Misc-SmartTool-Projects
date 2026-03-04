673ba1406fac937d7d13c5fef11ba07030151a63
MenaceMaster9600 <jm9876000@gmail.com>
2026-03-03 22:48:06 -0500

    Made builds... funnier

--- scripts/extract-and-build.js
+++ scripts/extract-and-build.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const babel = require('@babel/core');
+const { randomInt } = require('crypto');
 
 const repoRoot = path.join(__dirname, '..');
 const srcDir = path.join(repoRoot, 'src');
@@ -21,7 +22,10 @@ if (!fs.existsSync(srcPath)) {
 }
 
 const jsx = fs.readFileSync(srcPath, 'utf8');
+const selected = randomInt(1, 11);
 console.log('Read', srcPath);
+console.log('Transpiling JSX to plain JavaScript...');
+selected === 1 ? console.log('Watering Leafeon...') : selected === 2 ? console.log('Fluffing the Eevee...') : selected === 3 ? console.log('Keeping Umbreon safe...') : selected === 4 ? console.log('Giving lots of food to Flareon AND Jolteon...') : selected === 5 ? ('Fennekin ate the stick... the ram stick...') : selected === 6 ? console.log('Asking the guildmaster if femboys can join...') : selected === 7 ? console.log('My logs keep getting set on fire... can someone tell Flareon to stop?') : selected === 8 ? console.log('Did you know? 93% of all Vaporeon are actually Vaporeon!') : selected === 9 ? console.log('Did you know? 67% of the world\'s water comes from Vaporeon! That\'s approximately 2/3 of its whole water suppy! Wow!') : selected === 10 ? console.log('Did you know? Umbreon\'s sweat is actually poisonous! You wouldn\'t know it with how many followers they have...') : console.log('Adding Vaporeon...');
 
 // Transpile to plain JS bundle (single-file) using Babel programmatically
 let transformed;
@@ -38,6 +42,4 @@ try {
 
 const outPath = path.join(outDir, 'bundle.js');
 fs.writeFileSync(outPath, transformed.code, 'utf8');
-console.log('Built', outPath);
-
-console.log('Build complete. Note: this script no longer modifies mainVersion.html.');
+console.log('                                                                                                                                                                                                                                                Praise thy Vaporeon, for the following file hast built successfully:', outPath);
